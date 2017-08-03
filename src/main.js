1183b812922c558163f4af5d1872ec250347aff8
Leo Knudsen <nightskater-220@hotmail.com>
2017-08-03 19:02:15 +0200

    removed mistake in package.json

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@
     import ReactDOM from 'react-dom';
     import {Provider} from 'react-redux'
     import {Route, Router, HashHistory, BrowserHistory} from 'react-router'
-    import './css/app.css';
+    // import './css/app.css';
 
     const store = require('./store/storeConfig.jsx').configStoreWith({})
 
