732c118259eb0121ae8d0417be57d557720a6500
Leo Knudsen <nightskater-220@hotmail.com>
2017-08-03 18:18:57 +0200

    setup electron hot loading and watching, elctron reloading

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const BrowserWindow = electron.BrowserWindow
 
 let window
 
+require('electron-reload')(__dirname {
+	electron: require('electron-prebuilt'),
+	hardResetMethod:'exit'
+})
+
 const createWindow = () => {
 	window = new BrowserWindow({
 		height: 800,
@@ -11,6 +16,13 @@ const createWindow = () => {
 	})
 
 	window.loadURL('http://localhost:3999')
+
+	if (process.env.NODE_ENV === 'development') {
+	    const electronHot = require('electron-hot-loader');
+	    electronHot.install();
+	    electronHot.watchJsx(['src/**/*.jsx']);
+	    electronHot.watchCss(['src/assets/**/*.css']);
+	}
 }
 
 app.on('ready', createWindow)
