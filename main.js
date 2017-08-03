64a3581a7c85766a1ca7f09d312a3a6be64e2729
Leo Knudsen <nightskater-220@hotmail.com>
2017-08-03 15:56:08 +0200

    enable electron

---
+++ main.js
@@ -0,0 +1,17 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let window
+
+const createWindow = () => {
+	window = new BrowserWindow({
+		height: 800,
+		width:1280
+	})
+
+	window.loadURL('http://localhost:3999')
+
+}
+
+app.on('ready', createWindow)
